e638ec0d3f9bed1bb338353df4d35dc1dd1439ae
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-17 16:26:39 -0700

    Update practice file structure to incorporate different APIs
    Co-Authored by: Jessica Hattig <jessicahattig@gmail.com>
    Co-Authored by: Gareth Grindeland <gt.grindeland@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
-//This template is the 'entry point' and where UI Logic will go
-
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './js/triangle.js';
 
-function handleTriangleForm(event) {
-  event.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const triangle = new Triangle(length1, length2, length3);
-  const response = triangle.checkType();
-  const pTag = document.createElement("p");
-  pTag.append(response);
-  document.querySelector('#response').append(pTag);
+// Business Logic
+
+function getWeather(city) {
+  let request = new XMLHttpRequest();
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
+
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, city);
+    } else {
+      printError(this, response, city);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
 }
 
+// UI Logic
+
+function printElements(apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
+  The temperature in Fahernheit is ${apiResponse.main.temp} degrees. The current wind speed is ${apiResponse.wind.speed} mph. Current forecast is ${apiResponse.weather[0].main}.`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const city = document.querySelector('#location').value;
+  document.querySelector('#location').value = null;
+  getWeather(city);
+}
+
+function printError(request, apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+}
+
+
 window.addEventListener("load", function() {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
 });
